93175df4c8b04b95e95aa95183d9413e73338cd7
watataku11xx <watataku131122@outlook.jp>
2023-12-31 01:27:05 +0900

    add: NFTコントラクトの作成

--- MEMO.md
+++ MEMO.md
@@ -1,7 +1,9 @@
 ## WorkSpaceの使い方
 
 ### rootからpackage/contractのscript実行方法
-`npm run deploy -w packages/contract`
+```
+npm run deploy -w packages/contract
+```
 
-## MD参考文献
-Markdown記法一覧(https://qiita.com/oreo/items/82183bfbaac69971917f)
+## 参考文献
+[Markdown記法一覧](https://qiita.com/oreo/items/82183bfbaac69971917f)
--- packages/contract/contracts/MyEpicNFT.sol
+++ packages/contract/contracts/MyEpicNFT.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 import "hardhat/console.sol";
-contract MyEpicNFT {
-    constructor() {
-        console.log("This is my NFT contract.");
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+contract MyEpicNFT is ERC721URIStorage{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("TanyaNFT", "TANYA") {
+      console.log("This is my NFT contract.");
     }
+    
+    function makeAnEpikNFT() public {
+        _tokenIds.increment();
+        uint256 newItemd = _tokenIds.current();
+        _safeMint(msg.sender, newItemd);
+        _setTokenURI(newItemd, "https://api.npoint.io/f247131f11f141952605");
+        console.log('An NFT w/ ID %s has been minted to %s', newItemd, msg.sender);
+    }
+
 }
