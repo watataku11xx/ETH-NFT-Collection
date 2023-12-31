31bdf60f1c1f1091b362705d1d4f2fb19f279d46
watataku11xx <watataku131122@outlook.jp>
2023-12-31 14:58:46 +0900

    add: Base64エンコード処理をMyEpicNFT.solに追加

--- MEMO.md
+++ MEMO.md
@@ -8,8 +8,10 @@ npm run deploy -w packages/contract
 ## コントラクトアドレス
 ### TANYA(jpg画像)
 0x463711EcDfaF7243D032Dc9fe9c710044e16b7e4
-### AQUARE(SVG画像) 
+### SQUARE(SVG画像) 
 0xB2DF50fFD30AC9c4AAA5E864A6c2519209c8Cd99
+### ランダムSQUARE(SVG画像)
+0x4556Fba1Cb124e694E90752D2a9789D4c85f4Ec4
 
 ## 参考文献
 [Markdown記法一覧](https://qiita.com/oreo/items/82183bfbaac69971917f)
@@ -23,3 +25,5 @@ npm run deploy -w packages/contract
 [Etherscan(Sepolia)](https://sepolia.etherscan.io/)
 
 [SVG Viewer](https://www.svgviewer.dev/)
+
+[Snadbox](https://0y190.sse.codesandbox.io/)
--- packages/contract/contracts/MyEpicNFT.sol
+++ packages/contract/contracts/MyEpicNFT.sol
@@ -6,106 +6,128 @@ pragma solidity ^0.8.18;
 // いくつかの OpenZeppelin のコントラクトをインポートします。
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
 // utils ライブラリをインポートして文字列の処理を行います。
 import "@openzeppelin/contracts/utils/Counters.sol";
-
 import "hardhat/console.sol";
+
+// Base64.solコントラクトからSVGとJSONをBase64に変換する関数をインポートします。
+import { Base64 } from "./libraries/Base64.sol";
 
 // インポートした OpenZeppelin のコントラクトを継承しています。
 // 継承したコントラクトのメソッドにアクセスできるようになります。
 contract MyEpicNFT is ERC721URIStorage {
+    // OpenZeppelin　が　tokenIds　を簡単に追跡するために提供するライブラリを呼び出しています
+    using Counters for Counters.Counter;
+    // _tokenIdsを初期化（_tokenIds = 0）
+    Counters.Counter private _tokenIds;
 
-  // OpenZeppelin が tokenIds を簡単に追跡するために提供するライブラリを呼び出しています
-  using Counters for Counters.Counter;
+    // SVGコードを作成します。
+    // 変更されるのは、表示される単語だけです。
+    // すべてのNFTにSVGコードを適用するために、baseSvg変数を作成します。
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
-  // _tokenIdsを初期化（_tokenIds = 0）
-  Counters.Counter private _tokenIds;
+    // 3つの配列 string[] に、それぞれランダムな単語を設定しましょう。
+    string[] firstWords = ["Epic", "Fantastic", "Crude", "Crazy", "Hysterical", "Grand"];
+    string[] secondWords = ["Meta", "Live", "Pop", "Cute", "Sweet", "Hot"];
+    string[] thirdWords = ["Kitten", "Puppy", "Monkey", "Bird", "Panda", "Elephant"];
+    // NFT トークンの名前とそのシンボルを渡します。
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
+      console.log("This is my NFT contract.");
+    }
 
-  // SVGコードを作成します。
-  // 変更されるのは、表示される単語だけです。
-  // すべてのNFTにSVGコードを適用するために、baseSvg変数を作成します。
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    // シードを生成する関数を作成します。
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
+    }
 
-  // 3つの配列 string[] に、それぞれランダムな単語を設定しましょう。
-  string[] firstWords = ["Epic", "Fantastic", "Crude", "Crazy", "Hysterical", "Grand"];
-  string[] secondWords = ["Meta", "Live", "Pop", "Cute", "Sweet", "Hot"];
-  string[] thirdWords = ["Kitten", "Puppy", "Monkey", "Bird", "Panda", "Elephant"];
-  // NFT トークンの名前とそのシンボルを渡します。
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
-    console.log("This is my NFT contract.");
-  }
+    // 各配列からランダムに単語を選ぶ関数を3つ作成します。
+    // pickRandomFirstWord関数は、最初の単語を選びます。
+    function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+        // pickRandomFirstWord 関数のシードとなる rand を作成します。
+        uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+        // seed rand をターミナルに出力する。
+        console.log("rand - seed: ", rand);
+        // firstWords配列の長さを基準に、rand 番目の単語を選びます。
+        rand = rand % firstWords.length;
+        // firstWords配列から何番目の単語が選ばれるかターミナルに出力する。
+        console.log("rand - first word: ", rand);
+        return firstWords[rand];
+    }
 
-  // シードを生成する関数を作成します。
-  function random(string memory input) internal pure returns (uint256) {
-      return uint256(keccak256(abi.encodePacked(input)));
-  }
+    // pickRandomSecondWord関数は、2番目に表示されるの単語を選びます。
+    function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        // pickRandomSecondWord 関数のシードとなる rand を作成します。
+        uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
 
-  // 各配列からランダムに単語を選ぶ関数を3つ作成します。
-  // pickRandomFirstWord関数は、最初の単語を選びます。
-  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+    // pickRandomThirdWord関数は、3番目に表示されるの単語を選びます。
+    function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+        // pickRandomThirdWord 関数のシードとなる rand を作成します。
+        uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
 
-      // pickRandomFirstWord 関数のシードとなる rand を作成します。
-      uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+    // ユーザーが NFT を取得するために実行する関数です。
+    function makeAnEpicNFT() public {
 
-      // seed rand をターミナルに出力する。
-      console.log("rand seed: ", rand);
+        // NFT が Mint されるときのカウンターをインクリメントします。
+        _tokenIds.increment();
 
-      // firstWords配列の長さを基準に、rand 番目の単語を選びます。
-      rand = rand % firstWords.length;
+        // 現在のtokenIdを取得します。tokenIdは1から始まります。
+        uint256 newItemId = _tokenIds.current();
 
-      // firstWords配列から何番目の単語が選ばれるかターミナルに出力する。
-      console.log("rand first word: ", rand);
-      return firstWords[rand];
-  }
+        // 3つの配列からそれぞれ1つの単語をランダムに取り出します。
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
 
-  // pickRandomSecondWord関数は、2番目に表示されるの単語を選びます。
-  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+        // 3つの単語を連携して格納する変数 combinedWord を定義します。
+        string memory combinedWord = string(abi.encodePacked(first, second, third));
 
-      // pickRandomSecondWord 関数のシードとなる rand を作成します。
-      uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
-      rand = rand % secondWords.length;
-      return secondWords[rand];
-  }
+        // 3つの単語を連結して、<text>タグと<svg>タグで閉じます。
+        string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
 
-  // pickRandomThirdWord関数は、3番目に表示されるの単語を選びます。
-  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+	      // NFTに出力されるテキストをターミナルに出力します。
+	      console.log("\n----- SVG data -----");
+        console.log(finalSvg);
+        console.log("--------------------\n");
 
-      // pickRandomThirdWord 関数のシードとなる rand を作成します。
-      uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
-      rand = rand % thirdWords.length;
-      return thirdWords[rand];
-  }
+        // JSONファイルを所定の位置に取得し、base64としてエンコードします。
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // NFTのタイトルを生成される言葉（例: GrandCuteBird）に設定します。
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                        //  data:image/svg+xml;base64 を追加し、SVG を base64 でエンコードした結果を追加します。
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
 
-  // ユーザーが NFT を取得するために実行する関数です。
-  function makeAnEpicNFT() public {
+        // データの先頭に data:application/json;base64 を追加します。
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
 
-      // NFT が Mint されるときのカウンターをインクリメントします。
-      _tokenIds.increment();
+        console.log("\n----- Token URI ----");
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
 
-      // 現在のtokenIdを取得します。tokenIdは1から始まります。
-      uint256 newItemId = _tokenIds.current();
+        // msg.sender を使って NFT を送信者に Mint します。
+        _safeMint(msg.sender, newItemId);
 
-      // 3つの配列からそれぞれ1つの単語をランダムに取り出します。
-      string memory first = pickRandomFirstWord(newItemId);
-      string memory second = pickRandomSecondWord(newItemId);
-      string memory third = pickRandomThirdWord(newItemId);
+        // tokenURIを更新します。
+        _setTokenURI(newItemId, finalTokenUri);
 
-      // 3つの単語を連結して、<text>タグと<svg>タグで閉じます。
-      string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
-
-      // NFTに出力されるテキストをターミナルに出力します。
-      console.log("\n--------------------");
-      console.log(finalSvg);
-      console.log("--------------------\n");
-
-      // msg.sender を使って NFT を送信者に Mint します。
-      _safeMint(msg.sender, newItemId);
-
-      // tokenURI は後で設定します。
-      // 今は、tokenURI の代わりに、"We will set tokenURI later." を設定します。
-      _setTokenURI(newItemId, "We will set tokenURI later.");
-      // NFTがいつ誰に作成されたかを確認します。
-      console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
-  }
+        // NFTがいつ誰に作成されたかを確認します。
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    }
 }
